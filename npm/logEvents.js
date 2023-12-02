e645deff433e3862560c1c9667be5f85c02eec36
alsace7004 <alabibamidele14@gmail.com>
2023-12-02 04:20:11 +0100

    creating a web server

--- npm/logEvents.js
+++ npm/logEvents.js
@@ -6,7 +6,6 @@ const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 
-
 const logEvents = async (message)=>{
     const dateTime = `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
@@ -16,12 +15,10 @@ const logEvents = async (message)=>{
             await fsPromises.mkdir(path.join(__dirname,'logs'));  
         }
         await fsPromises.appendFile(path.join(__dirname,'logs','eventLog.txt'),logItem);
-       
     }catch(err){
         console.log(err)
     }
 }
-
 module.exports = logEvents
 
 /* console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'))
